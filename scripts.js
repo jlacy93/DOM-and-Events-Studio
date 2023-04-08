e5bcef0febde4abd7c481460b0d6fdbf0b50a24f
Josephina Lacy <josephinalacy@gmail.com>
2023-04-08 07:46:08 -0500

    Updated webpage interactability and movement of the shuttle and color of background changing

--- scripts.js
+++ scripts.js
@@ -4,22 +4,29 @@
 function init(){
 //confirm window
 //get element ID for take off button, flight status text, background, and height
-const takeOffButton = document.getElementById("takeoff")
-const flightStatusText = document.getElementById("flightStatus")
-const backgroundColor = document.getElementById("shuttleBackground")
-const height = document.getElementById("spaceShuttleHeight")
-const rocket = document.getElementById("rocket")
-rocket.style.position = 'absolute'
+const takeOffButton = document.getElementById("takeoff");
+const flightStatusText = document.getElementById("flightStatus");
+const backgroundColor = document.getElementById("shuttleBackground");
+const height = document.getElementById("spaceShuttleHeight");
+const rocket = document.getElementById("rocket");
+rocket.style.position = 'absolute';
+rocket.style.bottom = '0px';
+rocket.style.left = '50%';
+rocket.style.transform = 'translate(-50%, -50%)';
+let shuttleLeft = 50;
+let shuttleHeight = 0;
+let shuttleHeightPixels = 0;
 
-let shuttleHeight = 0
 
 //add listener on click of take off button
 takeOffButton.addEventListener("click", function (event) {
     let takeOff = confirm("Confirm that the shuttle is ready for takeoff.");
     if (takeOff) {
-        flightStatusText.innerHTML = "Shuttle in flight.";
-        backgroundColor.style.backgroundColor = '#2196f3'
-        shuttleHeight = 10000
+        shuttleHeight = 10000;
+        if(shuttleHeight == 0){backgroundColor.style.backgroundColor = 'Green'} else {
+            backgroundColor.style.backgroundColor = '#2196f3'};
+        shuttleHeightPixels = shuttleHeight * .0175;
+        rocket.style.bottom = shuttleHeightPixels+'px';
         spaceShuttleHeight.innerHTML = shuttleHeight }
     })
 
@@ -28,7 +35,11 @@ const landButton = document.getElementById("landing")
 landButton.addEventListener("click", function (event) {
     window.alert("The shuttle is landing. Landing gear engaged.")
     flightStatusText.innerHTML = "The shuttle has landed.";
-    shuttleHeight = 0
+    shuttleHeight = 0;
+    if(shuttleHeight == 0){backgroundColor.style.backgroundColor = 'Green'} else {
+        backgroundColor.style.backgroundColor = '#2196f3'};
+        shuttleHeightPixels = shuttleHeight * .0175;
+        rocket.style.bottom = shuttleHeightPixels+'px';
     spaceShuttleHeight.innerHTML = shuttleHeight
 })
 
@@ -38,8 +49,11 @@ abortButton.addEventListener("click", function (event){
     let confirmAbort = confirm("Confirm you want to abort the mission.");
     if (confirmAbort) {
         flightStatusText.innerHTML = "Mission Aborted.";
-        //background
-        shuttleHeight = 0
+        shuttleHeight = 0;
+        if(shuttleHeight == 0){backgroundColor.style.backgroundColor = 'Green'} else {
+            backgroundColor.style.backgroundColor = '#2196f3'};
+            shuttleHeightPixels = shuttleHeight * .0175;
+            rocket.style.bottom = shuttleHeightPixels+'px';
         spaceShuttleHeight.innerHTML = shuttleHeight
     }
 })
@@ -47,24 +61,31 @@ abortButton.addEventListener("click", function (event){
 //up button
 const upButton = document.getElementById("up")
 upButton.addEventListener("click", function (event){
-    shuttleHeight += 10
+    shuttleHeight += 10;
+    if(shuttleHeight == 0){backgroundColor.style.backgroundColor = 'Green'} else {
+        backgroundColor.style.backgroundColor = '#2196f3'};
+        shuttleHeightPixels = shuttleHeight * .0175;
+        rocket.style.bottom = shuttleHeightPixels+'px';
     spaceShuttleHeight.innerHTML = shuttleHeight
-    //move image
 })
 
 //down button
 const downButton = document.getElementById("down")
 downButton.addEventListener("click", function (event){
-    shuttleHeight -= 10
+    shuttleHeight -= 10;
+    if(shuttleHeight == 0){backgroundColor.style.backgroundColor = 'Green'} else {
+        backgroundColor.style.backgroundColor = '#2196f3'};
+        shuttleHeightPixels = shuttleHeight * .0175;
+        rocket.style.bottom = shuttleHeightPixels+'px';
     spaceShuttleHeight.innerHTML = shuttleHeight
-    //move image
 })
 
 
 //left button
 const leftButton = document.getElementById("left")
 downButton.addEventListener("click", function (event){
-    //move image
+    shuttleLeft += 1;
+    rocket.style.left = shuttleLeft+'%'
 })
 
 //right button
@@ -72,6 +93,7 @@ const rightButton = document.getElementById("right")
 downButton.addEventListener("click", function (event){
     //move image
 })
+
 
 }
 
